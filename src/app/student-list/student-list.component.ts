305e69c5a7924f2607eb76654e891ce24e40084a
Nishanth <nishanth.5.p@stackroute.in>
2019-05-31 15:59:33 +0530

    commit

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -13,10 +13,7 @@ export class StudentListComponent implements OnInit, OnDestroy {
   public students: any = {
     "request": {
       "filters": {
-        "userId": [
-          "2ea37912-ce7d-44c0-8816-34be186ad05c",
-          "6f324db7-32a5-4437-a451-35cf53269aaf"
-        ]
+        "userId": [ ]
       }
     }
   };
@@ -24,90 +21,31 @@ export class StudentListComponent implements OnInit, OnDestroy {
   private lstStudent: IStudent[] = [];
   private sltStudent: IStudent[] = [];
   private subscription: Subscription;
-  private messages = [];
-  // public Student = [
-  //   {
-  //     "userName": "jaswanth",
-  //     "emailVerified": "abc@gmail.com",
-  //     "firstName": "batch1",
-  //     "id": "B1"
-  //   },
-  //   {
-  //     "userName": "sai",
-  //     "emailVerified": "def@gmail.com",
-  //     "firstName": "batch1",
-  //     "id": "B1"
-  //   },
-  //   {
-  //     "userName": "balu",
-  //     "emailVerified": "balu@gmail.com",
-  //     "firstName": "batch1",
-  //     "id": "B1"
-  //   },
-  //   {
-  //     "userName": "jaswanth",
-  //     "emailVerified": "stackroute@gmail.com",
-  //     "firstName": "batch2",
-  //     "id": "B2"
-  //   },
-  //   {
-  //     "userName": "josh",
-  //     "emailVerified": "josh@gmail.com",
-  //     "firstName": "batch3",
-  //     "id": "B3"
-  //   },
-  //   {
-  //     "userName": "josh",
-  //     "emailVerified": "josh@gmail.com",
-  //     "firstName": "batch3",
-  //     "id": "B3"
-  //   },
-  //   {
-  //     "userName": "josh",
-  //     "emailVerified": "josh@gmail.com",
-  //     "firstName": "batch3",
-  //     "id": "B3"
-  //   },
-  //   {
-  //     "userName": "josh",
-  //     "emailVerified": "josh@gmail.com",
-  //     "firstName": "batch3",
-  //     "id": "B3"
-  //   },
-  //   {
-  //     "userName": "josh",
-  //     "emailVerified": "josh@gmail.com",
-  //     "firstName": "batch3",
-  //     "id": "B3"
-  //   },
-  // ];
+
   constructor(private _studentService: StudentService, private messageService: MessageService) { }
 
   ngOnInit() {
     this._studentService.getList(this.students, this._url).subscribe(data => { this.pushList(data); });
     this.subscription = this.messageService.getMessage().subscribe((payload) => {
-      this.messages.concat(Object.values(payload.payload));
-      console.log("values",Object.values(payload.payload));
-      this.subscribe();
+      console.log("values", Object.values(payload.payload));
+      this.subscribe(Object.values(payload.payload));
     });
   }
 
-  subscribe(){
-    // this.subscription = this.messageService.getMessage().subscribe((payload) => {
-    //   this.messages.concat(Object.values(payload));
-    // });
-
-    this.students = {
-      "request": {
-        "filters": {
-          "userId": [
-            this.messages
-          ]
+  subscribe(user) {
+    let userList = [];
+    let users:string;
+      user.map(x => x.forEach(x => userList.push(x)));
+      let userObj:any= {
+        "request": {
+          "filters": {
+            "userId":[   ]
+          }
         }
-      }
-    };
-    console.log("studentsfromBatch", this.students);
-    this.HttpRequest();
+      };
+      userObj.request.filters.userId = userList;
+      console.log("userIds:", userObj);
+      this.HttpRequest(userObj);
   }
 
   FieldsChange(student: IStudent, values: any) {
@@ -121,8 +59,8 @@ export class StudentListComponent implements OnInit, OnDestroy {
     }
   }
 
-  HttpRequest() {
-    this._studentService.getList(this.students, this._url).subscribe(data => { this.pushList(data); });
+  HttpRequest(students:object) {
+    this._studentService.getList(students, this._url).subscribe(data => { this.pushList(data); });
   }
 
   pushList(data: any) {
